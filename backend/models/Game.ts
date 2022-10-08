141737ee3b8af61b0609614073d48c35532db90c
Reyhan Muhammad Januar <reyhanmuhammadj@gmail.com>
2022-10-09 06:50:04 +0700

    added model schema for game

---
+++ backend/models/Game.ts
@@ -0,0 +1,16 @@
+import { Date, Schema, model } from 'mongoose';
+
+export interface Game {
+  address: string;
+  numberOfPeople: number;
+  date: Date;
+  time: string;
+  fieldNumber: number;
+}
+const GameSchema = new Schema<Game>({
+  address: { type: String, required: true },
+  numberOfPeople: { type: Number, required: true },
+  fieldNumber: { type: Number, required: true },
+});
+
+export default model<Game>('Game', GameSchema);
