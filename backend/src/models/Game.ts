75c735f615335fbc60159480c7f58c909c589e36
Reyhan Muhammad Januar <reyhanmuhammadj@gmail.com>
2022-10-09 07:18:45 +0700

    add field name to schema game

--- backend/src/models/Game.ts
+++ backend/src/models/Game.ts
@@ -1,16 +1,21 @@
 import { Date, Schema, model } from 'mongoose';
 
 export interface Game {
+  name: string;
   address: string;
   numberOfPeople: number;
   date: Date;
   time: string;
   fieldNumber: number;
 }
-const GameSchema = new Schema<Game>({
-  address: { type: String, required: true },
-  numberOfPeople: { type: Number, required: true },
-  fieldNumber: { type: Number, required: true },
-});
+const GameSchema = new Schema<Game>(
+  {
+    name: { type: String, required: true },
+    address: { type: String, required: true },
+    numberOfPeople: { type: Number, required: true },
+    fieldNumber: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
 
 export default model<Game>('Game', GameSchema);
