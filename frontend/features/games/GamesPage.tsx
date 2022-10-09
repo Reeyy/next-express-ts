8fa49d767e4a9ab7f404fa6892468b78d71a860c
Reyhan Muhammad Januar <reyhanmuhammadj@gmail.com>
2022-10-09 07:36:10 +0700

    set up store redux tool kit

---
+++ frontend/features/games/GamesPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function GamesPage() {
+  return (
+    <div>
+      <h2>games Page</h2>
+    </div>
+  );
+}
---
+++ frontend/store/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+export const store = configureStore({
+  reducer: {
+    games: GameReducer,
+  },
+});
+export type RootState = ReturnType<typeof store.getState>;
+export type AddDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AddDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
